6d43e67c4203ef11b04956460fa0dc8e000ce4f8
Aleksandr Dus <aleksandr.dus@scompler.com>
2023-03-25 21:56:28 +0300

    feat: add protect middleware

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -8,3 +8,33 @@ export const createJWT = (user) => {
 
   return token
 }
+
+export const protect = (req, res, next) => {
+  const bearer = req.headers.authorization
+
+  if (!bearer) {
+    res.status(401)
+    res.json({ message: 'Not authorized' })
+
+    return
+  }
+
+  const [, token] = bearer.split(' ')
+
+  if (!token) {
+    res.status(401)
+    res.json({ message: 'Invalid token' })
+
+    return
+  }
+
+  try {
+    const user = jwt.verify(token, process.env.JWT_SECRET)
+    req.user = user
+    next()
+  } catch (error) {
+    console.error(error)
+    res.status(401)
+    res.json({ message: 'Invalid token' })
+  }
+}
