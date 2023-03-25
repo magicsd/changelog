4012533c1c63fc95bc725de0f4270310f63de60d
Aleksandr Dus <aleksandr.dus@scompler.com>
2023-03-25 21:09:48 +0300

    feat: add morgan middleware

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import express from 'express'
+import morgan from 'morgan'
+
 import router from './router'
 
 const app = express()
+
+app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
   res.status(200)
