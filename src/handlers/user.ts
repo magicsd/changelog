21b1aada2ecb6d70ebd0f1839e1d1e2e97445193
Aleksandr Dus <aleksandr.dus@scompler.com>
2023-03-25 22:26:27 +0300

    feat: add signIn handler

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -1,5 +1,5 @@
 import prisma from "../db"
-import { createJWT, hashPassword } from "../modules/auth"
+import { comparePasswords, createJWT, hashPassword } from "../modules/auth"
 
 export const createNewUser = async (req, res) => {
   const user = await prisma.user.create({
@@ -8,6 +8,34 @@ export const createNewUser = async (req, res) => {
       password: await hashPassword(req.body.username)
     }
   })
+
+  const token = createJWT(user)
+
+  res.json({ token })
+}
+
+export const signIn = async (req, res) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      username: req.body.username
+    }
+  })
+
+  if (!user) {
+    res.json(401)
+    res.json({ message: 'User not found'})
+
+    return
+  }
+
+  const isValid = await comparePasswords(req.body.password, user?.password)
+
+  if (!isValid) {
+    res.json(401)
+    res.json({ message: 'Wrong password '})
+
+    return
+  }
 
   const token = createJWT(user)
 
